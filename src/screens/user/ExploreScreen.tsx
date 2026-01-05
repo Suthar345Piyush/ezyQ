22d127891ed51e0645bc840f0bcff65408f4b35d
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-01-05 16:49:00 +0530

    set template comp on screens

---
+++ src/screens/user/ExploreScreen.tsx
@@ -0,0 +1,15 @@
+import { YStack , Text } from "tamagui";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ExploreScreen() {
+    return (
+       <SafeAreaView style={{flex : 1}}>
+        <YStack f={1} jc="center" ai="center" bg="$background">
+          <Text fontSize="$8" fontWeight="bold" color="$gray12">
+            Explore queues
+          </Text>
+        </YStack>
+       </SafeAreaView>
+    );
+}
+
---
+++ src/screens/user/HomeScreen.tsx
@@ -0,0 +1,24 @@
+// user's home screen 
+
+import { YStack , Text , Button } from "tamagui";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
+export default function HomeScreen() {
+    return (
+       <SafeAreaView style={{flex : 1}}>
+        <YStack f={1} jc="center" ai="center" bg="$background" p="$6">
+          <Text fontSize="$8" fontWeight="bold" color="$gray12" mb="$4">
+             User Home Screen
+          </Text>
+
+          <Text color="$gray10" ta="center">
+             Coming Soon...
+          </Text>
+        </YStack>
+       </SafeAreaView>
+    );
+}
+
+
